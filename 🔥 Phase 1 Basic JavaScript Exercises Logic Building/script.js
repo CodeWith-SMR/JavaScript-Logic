5dbc92d305b2b1b0c899e3462d3c358c82fdf442
Muhammad Raza <169559718+CodeWith-SMR@users.noreply.github.com>
2025-03-18 09:02:50 +0500

    Update script.js

---
+++ 🔥 Phase 1 Basic JavaScript Exercises Logic Building/script.js
@@ -0,0 +1,9 @@
+let number = promet ("Enter Number");
+
+if(number %2 === 0){
+  console.log("Even Number")
+}else if(number %!2 === 0){
+  console.log("Odd Number")
+}else{
+  console.log( "please enter valid number")
+}
