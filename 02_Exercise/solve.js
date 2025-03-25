daa280062de2d6097a37809520319ff8530ab3ab
Muhammad Raza <169559718+CodeWith-SMR@users.noreply.github.com>
2025-03-25 19:03:54 +0000

    first commit

--- 02_Exercise/solve.js
+++ 02_Exercise/solve.js
@@ -4,18 +4,18 @@
 
 let marks = 95;
 
-if(marks >= 90 && marks <= 100) {
+if (marks >= 90 && marks <= 100) {
     console.log("Grade: A+");
-}else if(marks >= 80 && marks <= 90) {
+} else if (marks >= 80 && marks < 90) {  // `marks < 90` hona chahiye
     console.log("Grade: A");
-}else if(marks >= 70 && marks <= 80) {
+} else if (marks >= 70 && marks < 80) {  // `marks < 80` hona chahiye
     console.log("Grade: B"); 
-}else if(marks >= 60 && marks <= 70) {
+} else if (marks >= 60 && marks < 70) {  // `marks < 70` hona chahiye
     console.log("Grade: C"); 
-}else if(marks >= 50 && marks <= 60) {
+} else if (marks >= 50 && marks < 60) {  // `marks < 60` hona chahiye
     console.log("Grade: D"); 
-}else if(marks < 50 && marks >= 0) {
+} else if (marks < 50 && marks >= 0) {   // `marks < 50` hona chahiye
     console.log("Fail 😢"); 
-}else {
-    console.log("Enter a valid Marks");
+} else {
+    console.log("Enter valid marks (0-100)");
 }
