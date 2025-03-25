1d2237e9fc1a831361f617282b296ec165facf81
Muhammad Raza <169559718+CodeWith-SMR@users.noreply.github.com>
2025-03-25 01:36:59 +0000

    solve.js

--- 01_Exercise/solve.js
+++ 01_Exercise/solve.js
@@ -1,5 +1,12 @@
-let myName = "Muhammad Raza";
+let myName = "My name is Muhammad Raza";
 console.log(myName);
+
+
+let age = "I am 20y years old";
+console.log(age);
+
+
+let isStudent = true;
 
 
 //01_Exercise/solve.js
